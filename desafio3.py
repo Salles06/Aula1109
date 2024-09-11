8d9ff537d3e4ebe051824f9e5304311bf6449a87
Salles06 <joaovictorsalesdepaula@gmail.com>
2024-09-11 20:55:19 -0300

    Desafio Fatiar Strings

---
+++ desafio3.py
@@ -0,0 +1,8 @@
+# Desafio Fatiar Strings
+
+texto = "Eu me chamo João Victor Sales"
+tamanho = len(texto)
+print(texto[17:23])
+
+print(" O numero de elementos do nome é .:", tamanho)
+print(" A nova saída é .:", texto.split())
