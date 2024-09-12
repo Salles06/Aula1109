42bdc5785a338838e61363bd4e0f903b558ef040
Salles06 <joaovictorsalesdepaula@gmail.com>
2024-09-11 21:09:17 -0300

    Desafio Fatiar Strings outro modo correto

--- desafio3.py
+++ desafio3.py
@@ -1,8 +1,12 @@
 # Desafio Fatiar Strings
 
-texto = "Eu me chamo João Victor Sales"
-tamanho = len(texto)
-print(texto[17:23])
+nome = input("Digite seu nome. :")
+sobrenome = nome.split()
+#tamanho = len(nome)
 
-print(" O numero de elementos do nome é .:", tamanho)
-print(" A nova saída é .:", texto.split())
+if len(sobrenome) > 1:
+    print("O sobrenome digitado é.:", sobrenome [-1])
+else:
+    print(" Não foi digitado nome com sobrenomes")
+#print(" O numero de elementos do nome é .:", tamanho)
+#print(" A nova saída é .:", texto.split())
